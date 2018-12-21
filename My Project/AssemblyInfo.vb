7c876c6730874d1e4bb23e447f485306661d8c6b
jaegerbo <Bodo_Jaeger@Web.de>
2018-12-21 11:27:26 +0100

    Projektdateien hinzufügen.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("TranslatorTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Michael Weinig AG")>
+<Assembly: AssemblyProduct("TranslatorTests")>
+<Assembly: AssemblyCopyright("Copyright © Michael Weinig AG 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("0b8f979c-9fc9-4d35-a83a-4950f5c2fb3d")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ clsTest.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Translator
+
+<TestClass()>
+Public Class clsTest
+
+   <TestMethod()>
+   Public Sub TextLadenTesten()
+      Try
+         Dim Text As String = "bedGroupDesc,blocks,Block,EnChanged,Placing it on the ground will let you spawn there when you die. It shows on your map and prevents zombie respawn. Only the last placed one will be active.,""En le plaçant à terre, vous pourrez réapparaître à cet endroit lorsque vous mourrez et il sera indiqué sur votre carte."",""Wenn du dies platzierst, kannst du dort wiedererscheinen, falls du stirbst. Außerdem wird der Standort auf deiner Karte angezeigt."""
+
+         Dim TextManager As New clsTextDataManager
+         Dim list As ArrayList = TextManager.createDatasetRow(Text)
+         Assert.AreEqual("bedGroupDesc", list(0), "Spalte 0 falsch eingelesen")
+         Assert.AreEqual("blocks", list(1), "Spalte 1 falsch eingelesen")
+         Assert.AreEqual("Block", list(2), "Spalte 2 falsch eingelesen")
+         Assert.AreEqual("EnChanged", list(3), "Spalte 3 falsch eingelesen")
+         Assert.AreEqual("Placing it on the ground will let you spawn there when you die. It shows on your map and prevents zombie respawn. Only the last placed one will be active.", list(4), "Spalte 4 falsch eingelesen")
+         Assert.AreEqual("En le plaçant à terre, vous pourrez réapparaître à cet endroit lorsque vous mourrez et il sera indiqué sur votre carte.", list(5), "Spalte 5 falsch eingelesen")
+         Assert.AreEqual("Wenn du dies platzierst, kannst du dort wiedererscheinen, falls du stirbst. Außerdem wird der Standort auf deiner Karte angezeigt.", list(6), "Spalte 6 falsch eingelesen")
+
+         Text = "airConditionVentFanDouble,blocks,Block,New,""Vent Fan, Double"",,,,,"
+         list = TextManager.createDatasetRow(Text)
+         Assert.AreEqual("airConditionVentFanDouble", list(0), "Spalte 0 falsch eingelesen")
+         Assert.AreEqual("blocks", list(1), "Spalte 1 falsch eingelesen")
+         Assert.AreEqual("Block", list(2), "Spalte 2 falsch eingelesen")
+         Assert.AreEqual("New", list(3), "Spalte 3 falsch eingelesen")
+         Assert.AreEqual("Vent Fan, Double", list(4), "Spalte 4 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(5), "Spalte 5 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(6), "Spalte 6 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(7), "Spalte 7 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(8), "Spalte 8 falsch eingelesen")
+
+         Text = "cntBagsRandomLootHelperDesc,blocks,Block,New,""Will randomly spawn bags, backpacks, and purses (50%), small trash (25%) or nothing (25%)."",,,,,"
+         list = TextManager.createDatasetRow(Text)
+         Assert.AreEqual("cntBagsRandomLootHelperDesc", list(0), "Spalte 0 falsch eingelesen")
+         Assert.AreEqual("blocks", list(1), "Spalte 1 falsch eingelesen")
+         Assert.AreEqual("Block", list(2), "Spalte 2 falsch eingelesen")
+         Assert.AreEqual("New", list(3), "Spalte 3 falsch eingelesen")
+         Assert.AreEqual("Will randomly spawn bags, backpacks, and purses (50%), small trash (25%) or nothing (25%).", list(4), "Spalte 4 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(5), "Spalte 5 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(6), "Spalte 6 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(7), "Spalte 7 falsch eingelesen")
+         Assert.AreEqual(String.Empty, list(8), "Spalte 8 falsch eingelesen")
+      Catch ex As Exception
+         Assert.Fail(ex.Message)
+      End Try
+   End Sub
+
+End Class
